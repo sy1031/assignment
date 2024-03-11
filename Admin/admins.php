c0e084297407a20542c6ec028fd5f28246925c66
sy1031 <shinyibeh31@gmail.com>
2024-03-11 21:31:36 +0800

    latest

--- Admin/admins.php
+++ Admin/admins.php
@@ -1,13 +1,14 @@
-<?php include('includes/header.php'); ?>
+<?php include('Includes/header.php'); ?>
 
 <div class="container-fluid px-4">
-    <div class="card">
+    <div class="card mt-4 shadow-sm">
         <div class="card-header">
-            <h4 class="mb-0">Admins
-                <a href="admins_create.php" class="btn btn-primary float-end">Add Admin</a>
+            <h4 class="mb-0">Staff List
+                <a href="admins_create.php" class="btn btn-primary float-end">Add Staff</a>
             </h4>
         </div>
         <div class="card-body">
+            <?php alertMessage(); ?>
             <div class="table-responsive">
                 <table class="table table-striped table-bordered">
                     <thead>
@@ -20,11 +21,33 @@
                     </thead>
 
                     <tbody>
-                        <tr>
-                            <td>
+                        <?php
+                        $staff = getAll('staff');
+                        if(mysqli_num_rows($staff)>0){
 
+                        
+                        ?>
+                        <?php foreach($staff as $staffItem) : ?>
+                        <tr>
+                            <td><?= $staffItem['id'] ?></td>
+                            <td><?= $staffItem['name'] ?></td>
+                            <td><?= $staffItem['email'] ?></td>
+                            <td>
+                                <a href="staff_edit.php" class="btn btn-danger btn-sm">Edit</a>
+                                <a href="staff_delete.php" class="btn btn-danger btn-sm">Delete</a>
                             </td>
                         </tr>
+                        <?php endforeach; ?>
+                        <?php
+                        }
+                        else{
+                            ?>
+                            <tr>
+                                <td colspan="4"> No Record found</td>
+                            </tr>
+                            <?php
+                        } 
+                        ?>
                     </tbody>
                 </table>
             </div>
@@ -32,3 +55,4 @@
     
     </div>
 </div>
+<?php include('Includes/footer.php'); ?>
--- index.php
+++
@@ -1,5 +0,0 @@
-<?php include('includes/header.php'); ?>
-    <h1>Hello, world!</h1>
-
-<?php include('includes/footer.php'); ?>
-    
