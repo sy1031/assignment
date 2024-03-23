f82c1a7c9ca3f8dc5cad2f09155a6f651fad535d
Liew Xin Yu <liewxinyuuu@gmail.com>
2024-03-23 17:46:35 +0800

    update

--- Admin/category_create.php
+++ Admin/category_create.php
@@ -16,20 +16,20 @@
             <div class="row">
                 <div class="col-md-12 mb-3">
                     <label for="">Name</label>
-                    <input type="text" name="name" required class="form-control" />
+                    <input type="text" name="categoryName" required class="form-control" />
                 </div>
 
                 <div class="col-md-12 mb-3">
                     <label for="">Description</label> <!--Description can be null--->
-                    <textarea name="description" class="form-control" rows="3"></textarea>
+                    <textarea name="categoryDescription" class="form-control" rows="3"></textarea>
                 </div>
 
                 <div class="col-md-6"> <!--To hide categories that you don't want to delete but want it to be unvisible to users--->
                     <label for="">Status(UnChecked = Visible, Checked = Hidden)</label>
                     <br/>
-                    <input type="checkbox" name="status" style="width:30px;height:30px";>
+                    <input type="checkbox" name="categoryStatus" style="width:30px;height:30px";>
                 </div>
-
+categoryStatus
                 <div class="col-md-6 mb-3 text-end">
                     <br/>
                     <button type="submit" name="saveCategory" class="btn btn-primary">Save</button>
