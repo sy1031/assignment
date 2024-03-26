8cb9ca80835123f0482d9904ac4f9696663ea918
Liew Xin Yu <liewxinyuuu@gmail.com>
2024-03-26 17:53:14 +0800

    update

--- Admin/category_create.php
+++ Admin/category_create.php
@@ -3,79 +3,36 @@
 <div class="container-fluid px-4">
     <div class="card mt-4 shadow-sm">
         <div class="card-header">
-            <h4 class="mb-0">Add Products
-                <a href="product.php" class="btn btn-primary float-end">Back</a>
+            <h4 class="mb-0">Create Category
+                <a href="category.php" class="btn btn-primary float-end">Back</a>
             </h4>
         </div>
         <div class="card-body">
 
             <?php alertMessage(); ?>
 
-            <form action="code.php" method="POST" enctype="multipart/form-data">
+            <form action="code.php" method="POST">
 
             <div class="row">
                 <div class="col-md-12 mb-3">
-                    <label>Select Category</label>
-                    <select name="category_ID" class="form-select">
-                        <option value="">Select Category</option>
-                            <?php
-                            $category=getAll('category');
-                            if($category){
-                                if(mysqli_num_rows($category) > 0){
-                                    foreach($category as $catitem){
-                                        echo '<option value="'.$catitem['category_ID'].'">'.$catitem['categoryName'].'</option>';
-                                    }
-
-                                }else{
-                                    echo '<option value="">No Categories Found!</option>';
-                                }
-                                
-                            }else{
-                                echo '<option value="">Something Went Wrong!</option>';
-                            }
-                            ?>
-                    </select>
-                </div>
-                <div class="col-md-6 mb-3">
-                    <label for="">Product Name</label>
-                    <input type="text" name="productName" required class="form-control" />
-                </div>
-
-                <div class="col-md-6 mb-3"> <!--new--->
-                    <label for="">Brand</label>
-                    <input type="text" name="productBrand" required class="form-control" />
+                    <label for="">Name</label>
+                    <input type="text" name="categoryName" required class="form-control" />
                 </div>
 
                 <div class="col-md-12 mb-3">
-                    <label for="">Description</label>
-                    <textarea name="productDescription" required class="form-control" rows="3"></textarea>
+                    <label for="">Description</label> <!--Description can be null--->
+                    <textarea name="categoryDescription" class="form-control" rows="3"></textarea>
                 </div>
 
-
-                <div class="col-md-4 mb-3">
-                    <label for="">Price</label>
-                    <input type="text" name="productPrice" required class="form-control" />
-                </div>
-
-                <div class="col-md-4 mb-3">
-                    <label for="">Quantity</label>
-                    <input type="text" name="productQuantity" required class="form-control" />
-                </div>
-
-                <div class="col-md-4 mb-3">
-                    <label for="">Image</label>
-                    <input type="file" name="productImage" class="form-control" />
-                </div>
-
-                <div class="col-md-6">
+                <div class="col-md-6"> <!--To hide categories that you don't want to delete but want it to be unvisible to users--->
                     <label for="">Would you like to hide the inserted information?</label>
                     <br/>
-                    <input type="checkbox" name="productAvailability" style="width:30px;height:30px";>
+                    <input type="checkbox" name="categoryStatus" style="width:30px;height:30px";>
                 </div>
 
                 <div class="col-md-6 mb-3 text-end">
                     <br/>
-                    <button type="submit" name="saveProduct" class="btn btn-primary">Save</button>
+                    <button type="submit" name="saveCategory" class="btn btn-primary">Save</button>
                 </div>
             </div>
 
