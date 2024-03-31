cd355637814142d71431f636beafc941aaf5362f
Liew Xin Yu <liewxinyuuu@gmail.com>
2024-03-31 14:50:19 +0800

    update

--- staff/Includes/sidebar.php
+++ staff/Includes/sidebar.php
@@ -5,12 +5,32 @@
             <div class="nav">
                 <div class="sb-sidenav-menu-heading text-dark">Interface</div>
 
-                <a class="nav-link text-dark collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts"
-                    aria-expanded="false" aria-controls="collapseLayouts">
-                    <div class="sb-nav-link-icon"><i class="fas fa-box text-dark"></i></div>
-                    Product
+                <!--Categories--->
+                <a class="nav-link text-dark collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseCategory" aria-expanded="false" aria-controls="collapseCategory">
+                    <div class="sb-nav-link-icon"><i class="fas fa-shopping-basket text-dark"></i></div>
+                    Categories
                     <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down text-dark"></i></div>
                 </a>
+                <div class="collapse" id="collapseCategory" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                    <nav class="sb-sidenav-menu-nested nav">
+                        <a class="nav-link text-dark" href="category_create.php">Create Category</a>
+                        <a class="nav-link text-dark" href="category.php">View Categories</a>
+                    </nav>
+                </div>
+
+                <!--Products--->
+                <a class="nav-link text-dark collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseProduct" aria-expanded="false" aria-controls="collapseProduct">
+                    <div class="sb-nav-link-icon"><i class="fas fa-box text-dark"></i></div>
+                    Products
+                    <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down text-dark"></i></div>
+                </a>
+                <div class="collapse" id="collapseProduct" data-bs-target="#collapseProduct" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                    <nav class="sb-sidenav-menu-nested nav">
+                        <a class="nav-link text-dark" href="product_create.php">Create Product</a>
+                        <a class="nav-link text-dark" href="product.php">View Products</a>
+                    </nav>
+                </div>
+
 
                 <a class="nav-link text-dark collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseOrders"
                     aria-expanded="false" aria-controls="collapseAdmins">
